51500b0a1ef2f017b32221423e3d16a6782fd2bb
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2015-12-09 14:02:49 -0500

    example

--- index.js
+++ index.js
@@ -1,32 +1,26 @@
+var spec = process.argv[2];
+
 var Mocha = require('mocha')
 var mocha = new Mocha()
 var shuffle = require('lodash.shuffle')
 
-mocha.addFile('./spec.js');
+mocha.addFile(spec);
 
 function shuffleTests(suite) {
-  console.log('shuffling %d unit tests in "%s"',
-    suite.tests.length, suite.title);
-  suite.tests = shuffle(suite.tests);
+  if (suite.tests.length) {
+    console.log('shuffling %d unit tests in "%s"',
+      suite.tests.length, suite.title);
+    suite.tests = shuffle(suite.tests);
+  }
   suite.suites.forEach(shuffleTests);
 }
 
 mocha.suite.beforeAll(function () {
-  console.log('before all suite event')
-  console.log('total %d tests', mocha.suite.total())
-  console.log('first suite title "%s"', mocha.suite.fullTitle());
-
   shuffleTests(mocha.suite);
-
-  console.log('has inside', mocha.suite.suites.map(function (s) {
-    return s.fullTitle()
-  }))
 })
 
 mocha.run(function (failures) {
-  console.log('mocha run finished with %d failures', failures)
-
   process.on('exit', function () {
-    process.exit(failures)
+    process.exit(failures);
   })
 })
--- spec/spec.js
+++ spec/spec.js
@@ -1,28 +1,8 @@
-// ./node_modules/mocha/bin/mocha spec.js
-// console.log('mocha', mocha);
+describe('example', function () {
 
-// mocha.events.on('suite', function () {
-//   console.log('suite', arguments)
-// })
+  it('runs test 1', function () {})
 
+  it('runs test 2', function () {})
 
-describe('same tests', function () {
-
-  console.log('inside same tests describe');
-
-  // beforeAll(function () {
-  //   console.log('before all')
-  // })
-
-  it('runs test 1', function () {
-    console.log('test 1')
-  })
-
-  it('runs test 2', function () {
-    console.log('test 2')
-  })
-
-  it('runs test 3', function () {
-    console.log('test 3')
-  })
+  it('runs test 3', function () {})
 })
