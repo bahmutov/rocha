9ba652100c326a2c5ff1647b8adf7fa2de6aaa87
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2015-12-09 13:57:50 -0500

    example

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 var Mocha = require('mocha')
 var mocha = new Mocha()
+var shuffle = require('lodash.shuffle')
 
 mocha.addFile('./spec.js');
+
+function shuffleTests(suite) {
+  console.log('shuffling %d unit tests in "%s"',
+    suite.tests.length, suite.title);
+  suite.tests = shuffle(suite.tests);
+  suite.suites.forEach(shuffleTests);
+}
 
 mocha.suite.beforeAll(function () {
   console.log('before all suite event')
   console.log('total %d tests', mocha.suite.total())
   console.log('first suite title "%s"', mocha.suite.fullTitle());
+
+  shuffleTests(mocha.suite);
+
   console.log('has inside', mocha.suite.suites.map(function (s) {
     return s.fullTitle()
   }))
