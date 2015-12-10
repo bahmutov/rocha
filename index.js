425ed5f72185bd236c34143d26d60e19197856d2
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2015-12-09 23:09:49 -0500

    chore(load): working on loading and restoring the previous test ordering

--- index.js
+++ index.js
@@ -1,34 +1,60 @@
 const Mocha = require('mocha')
 const mocha = new Mocha()
-const shuffle = require('lodash.shuffle')
+const _ = require('lodash')
 const log = require('debug')('rocha')
 
 function shuffleTests (suite) {
   if (suite.tests.length) {
     log('shuffling %d unit tests in "%s"',
       suite.tests.length, suite.title)
-    suite.tests = shuffle(suite.tests)
+    suite.tests = _.shuffle(suite.tests)
   }
   suite.suites.forEach(shuffleTests)
 }
 
-function testOrder (suite) {
-  return [{
+function collectSuite (suite, collected) {
+  collected.push({
     title: suite.fullTitle(),
-    tests: suite.tests.map(t => t.title),
-    suites: suite.suites.map(testOrder)
-  }]
+    tests: suite.tests.map(t => t.title)
+  })
+  suite.suites.forEach(s => collectSuite(s, collected))
+}
+
+function collectTestOrder (rootSuite) {
+  const suites = []
+  collectSuite(rootSuite, suites)
+  return suites
+}
+
+function setOrder (suite, order) {
+  const foundInfo = _.find(order, { title: suite.fullTitle() })
+  if (foundInfo) {
+    log('restoring order to', foundInfo.title)
+  }
+  suite.suites.forEach(s => setOrder(s, order))
 }
 
 const join = require('path').join
 const filename = join(process.cwd(), '.rocha.json')
 
 function saveOrder (suite) {
-  const order = testOrder(suite)
+  const order = collectTestOrder(suite)
   const json = JSON.stringify(order, null, 2)
   const save = require('fs').writeFileSync
   save(filename, json)
   log('saved order to file', filename)
+}
+
+function loadOrder () {
+  const exists = require('fs').existsSync
+  if (!exists(filename)) {
+    return
+  }
+  const load = require('fs').readFileSync
+  const json = load(filename)
+  const order = JSON.parse(json)
+  log('loaded order from', filename)
+  return order
 }
 
 function rocha (options) {
@@ -47,8 +73,16 @@ function rocha (options) {
   specFilenames.forEach(mocha.addFile.bind(mocha))
 
   mocha.suite.beforeAll(function () {
-    shuffleTests(mocha.suite)
-    saveOrder(mocha.suite)
+    const order = loadOrder()
+    if (order) {
+      // how to make sure we ran with
+      // the same set of test files?
+      log('reordering specs like last time')
+      setOrder(mocha.suite, order)
+    } else {
+      shuffleTests(mocha.suite)
+      saveOrder(mocha.suite)
+    }
   })
 
   mocha.run(function (failures) {
